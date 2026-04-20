940e4e56f4d7009b9ed0e0e5155c9ce20275d747
junbyeol <yoonjs0510@gmail.com>
2026-04-20 21:53:47 +0900

    chore: 마지막 업데이트 일 수정

--- src/locales/lastUpdated.ts
+++ src/locales/lastUpdated.ts
@@ -1,4 +1,4 @@
-export const lastUpdated = new Date("2026-04-15").toLocaleDateString();
+export const lastUpdated = new Date("2026-04-20").toLocaleDateString();
 export const lastUpdatedNowDaysInterval = Math.floor(
   (new Date().getTime() - new Date(lastUpdated).getTime()) /
     (1000 * 60 * 60 * 24),
